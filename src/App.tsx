864f408c88359fcd0cbc8d5d49376c0f0d846917
Neel <neelpatel805804@gmail.com>
2023-09-19 01:40:03 +0530

    project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,83 @@
+import Home from "./pages/Home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Users from "./pages/Users/Users";
+import Products from "./pages/Products/Proudcts";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Menu from "./components/Menu/Menu";
+import Login from "./pages/Login/Login";
+import"./styles/global.scss"
+import User from "./pages/user/User";
+import Product from "./pages/product/Product";
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+
+
+
+const queryClient = new QueryClient()
+
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+          <QueryClientProvider client={queryClient}>
+            <Outlet />
+            </QueryClientProvider>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+
+        {
+          path: "/users",
+          element: <Users />
+        }, {
+          path: "/products",
+          element: <Products />
+        },
+        {
+          path: "/users/:id",
+          element: <User />
+        },
+        {
+          path: "/products/:id",
+          element: <Product />
+        },
+      ]
+    },
+    {
+      path:"/login",
+      element:<Login/>
+    }
+  ]);
+
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <span>
+        novaadmin
+      </span>
+      <span>© Nova Admin Dashboard</span>
+      </div>
+  )
+}
+
+export default Footer;
